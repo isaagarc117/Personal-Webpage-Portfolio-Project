3504eda238d3192d3f7b50bcdb5832b85e36b306
isaagarc117 <90589049+isaagarc117@users.noreply.github.com>
2023-07-07 15:58:26 -0400

    Added a reset button which changes the elements with the class = turquoise back to their original color. The button is normally hidden. When the elements change color, it is unhidden. Whne the button is clicked, it resets the color as stated before. The issue now is that after the button is clicked it tis hidden, but when one changes the color of the elements again, the button no longer unhides.

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 //Event targets
 const changesColor = document.getElementsByClassName("turquoise");
 
+const resetButton = document.getElementsByClassName("button")[0];
+
 //=======================================================================================
 
 //Event Functions
 
 //This function assigns an event listener to a target
 function assignEventsToTurquoiseArrayElements(element){
+
     element.addEventListener('mouseover', changeColor);
+    
 };
 
 // This random number function will create color codes for the randomColor variable
@@ -17,13 +21,40 @@ function colorValue() {
 
 //this function changes the color of the element to which the event is attached to
 function changeColor(event){
+    const originalColor = 'rgb(8, 185, 185)';
     let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
     event.target.style.color = randomColor;
+
+    if(originalColor !== randomColor){
+        unhideButton();
+    }
+};
+
+
+//funciton that unhides the button once the color has changed 
+function unhideButton(){
+    resetButton.style.display = 'flex';
+};
+
+
+//function that resets the colors back to original and hides the reset button
+function resetColor(event){
+
+    for(i=0;i<changesColorArray.length;i++){
+        const originalColor = 'rgb(8, 185, 185)';
+        changesColorArray[i].style.color = originalColor;
+    }
+
+    event.target.style.display = 'none';
+
 };
 
 //=======================================================================================
 
 //Event Listeners
+
+
+//Event Listeners associated with the class = 'turquoise' elements
 
 //changesColor is an HTML collection, which will not work like an array, must convert the collection to an 
 //array first.
@@ -31,3 +62,8 @@ var changesColorArray = [].slice.call(changesColor);
 
 //Now we should be able to iterate through the converted array 
 changesColorArray.forEach(assignEventsToTurquoiseArrayElements);
+
+
+
+//Event Listeners associated with the reset button
+resetButton.addEventListener('click', resetColor);
