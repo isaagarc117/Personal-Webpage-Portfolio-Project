b4e49a747bfe560c835397e48911b6c5fb138cd9
isaagarc117 <90589049+isaagarc117@users.noreply.github.com>
2023-07-07 17:38:09 -0400

    Added the button that disables the color change functionality. The page works well. This is the final commit for now

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-//Event targets
+/*PERSONAL PORTFOLIO PROJECT - JAVASCRIPT*/ 
+
+//Event targets 
 const changesColor = document.getElementsByClassName("turquoise");
 
-const resetButton = document.getElementsByClassName("button")[0];
+const buttonContainer = document.getElementsByClassName("button-container")[0];
+
+const resetButton = document.getElementById("reset-button");
+
+const disableButton = document.getElementById("disable-color-change-button");
 
 //=======================================================================================
 
-//Event Functions
+//Global Functions - Not directly associated to event listeners
+
+// This random number function will create color codes for the randomColor variable
+function colorValue() {
+    return Math.floor(Math.random() * 256);
+  };
+
+
+//funciton that unhides the button once the color has changed 
+function unhideButton(){
+    buttonContainer.style.display = 'flex';
+};
+
+
+//function that resets the colors back to original and hides the reset button
+function resetColor(){
+
+    for(i=0;i<changesColorArray.length;i++){
+        const originalColor = 'rgb(8, 185, 185)';
+        changesColorArray[i].style.color = originalColor;
+    }
+
+    buttonContainer.style.display = 'none';
+
+};
+
+//=======================================================================================
+
+//Event Functions - either require an element (iteration taking place) or an event argument
 
 //This function assigns an event listener to a target
 function assignEventsToTurquoiseArrayElements(element){
@@ -14,10 +48,12 @@ function assignEventsToTurquoiseArrayElements(element){
     
 };
 
-// This random number function will create color codes for the randomColor variable
-function colorValue() {
-    return Math.floor(Math.random() * 256);
-  };
+//This function deletes even listeners from a target
+function deleteEventsFromTurquoiseArrayElements(element){
+    element.removeEventListener('mouseover', changeColor);
+}
+
+
 
 //this function changes the color of the element to which the event is attached to
 function changeColor(event){
@@ -30,23 +66,13 @@ function changeColor(event){
     }
 };
 
+//function that disables the color change functionality
+function disableColorChange(event){
 
-//funciton that unhides the button once the color has changed 
-function unhideButton(){
-    resetButton.style.display = 'flex';
-};
+    //disable the event listener on all turquoise array elements
+    changesColorArray.forEach(deleteEventsFromTurquoiseArrayElements);
 
-
-//function that resets the colors back to original and hides the reset button
-function resetColor(event){
-
-    for(i=0;i<changesColorArray.length;i++){
-        const originalColor = 'rgb(8, 185, 185)';
-        changesColorArray[i].style.color = originalColor;
-    }
-
-    resetButton.style.display = 'none';
-
+    resetColor();
 };
 
 //=======================================================================================
@@ -64,6 +90,9 @@ var changesColorArray = [].slice.call(changesColor);
 changesColorArray.forEach(assignEventsToTurquoiseArrayElements);
 
 
-
 //Event Listeners associated with the reset button
 resetButton.addEventListener('click', resetColor);
+
+
+//Event Listeners associated with the disable button
+disableButton.addEventListener('click', disableColorChange);
